8588400229cfd9217610febd538f56f87be4b3bf
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-03-03 19:07:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,13 +20,14 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
-  def reverse_transfer 
-    if @status = 'complete' && valid? == true && @receiver.balance > @amount
-    @receiver.balance -= @amount 
-    @sender.balance += @amount 
-    @status = 'reversed'
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
     else 
       @status = 'rejected' 
+      return "Transaction rejected. Please check your account balance."
       
     end 
   end
